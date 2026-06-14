6c5f6663862f6fff847cd662dfe02028cf3bc247
GardenMovie <dodygardenmovie@gmail.com>
2026-06-13 22:46:28 -0300

    Chore: update .env loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
   server: {
     port: 3000,
     proxy: {
-      "/api": process.env.VITE_API_URL ?? "http://localhost:8000",
+      "/api": {
+        target: env.VITE_API_URL ?? "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
     },
   },
   plugins: [react(), tailwindcss()],
@@ -17,4 +23,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  }
 })
